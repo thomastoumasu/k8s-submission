2c522d5197f7819d587f6f735220b13ae94ab56a
thomastoumasu <thomastoumasu@protonmail.com>
2025-11-25 16:03:42 +0100

    corrected typo

--- the_project/bin/bash/ex1_5.sh
+++ the_project/bin/bash/ex1_5.sh
@@ -1,10 +1,12 @@
 # k3d cluster create -a 2
 
+# kubectl delete -f manifests/deployment.yaml
+
 sh ../builder.sh the_project 1.5
 
 kubectl apply -f manifests/deployment.yaml
 
-POD=$(kubectl get pods -o=name | grep the_project)
+POD=$(kubectl get pods -o=name | grep the-project)
 
 kubectl wait --for=condition=Ready $POD
 
