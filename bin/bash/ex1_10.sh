b61c018a4557d9a52b50d242dcac6268cd22471d
thomastoumasu <thomastoumasu@protonmail.com>
2025-11-26 16:07:28 +0100

    added comments

--- bin/bash/ex1_10.sh
+++ bin/bash/ex1_10.sh
@@ -1,4 +1,4 @@
-# ingress service - to access app from outside
+# ingress service - to access multiple apps (here two: log_output and pingpong) from outside
 sh clear.sh  
 
 # # create image of pingpong
@@ -13,19 +13,21 @@ sh clear.sh
 # create cluster
 k3d cluster create -p 8081:80@loadbalancer --agents 2
 
+# create deployment and service for both apps and common ingress
 kubectl apply -f ./log_output/manifests/deployment.yaml
 kubectl apply -f ./log_output/manifests/service.yaml
 kubectl apply -f ./pingpong/manifests/deployment.yaml
 kubectl apply -f ./pingpong/manifests/service.yaml
 kubectl apply -f manifests/ingress.yaml
 
-# check that app is accessible on host port
+# check apps are accessible on host port
 kubectl rollout status deployment log-output-dep
 kubectl rollout status deployment pingpong-dep
-
-sleep 10 
+POD1=$(kubectl get pods -o=name | grep log)
+POD2=$(kubectl get pods -o=name | grep pingpong)
+kubectl wait --for=condition=Ready $POD1 $POD2
+sleep 30 
 curl localhost:8081
 curl localhost:8081/pingpong
 
-# check, should see the svc on 2345 and 1234 as well as the ingress on 80
-# kubectl get svc,ing
+# kubectl get svc,ing # should see the svc on 2345 and 1234 as well as the ingress on 80
