b4275db0f2ba6242d4b14c26eddc00416c6298ff
thomastoumasu <thomastoumasu@protonmail.com>
2025-12-16 11:10:00 +0100

    github actions

--- create_image.sh
+++ create_image.sh
@@ -8,3 +8,8 @@ docker build --platform linux/amd64 -t 3.4 .
 # docker run --rm -p 3000:3000 --name 2.10 2.10 && curl localhost:8082 
 docker tag 3.4 thomastoumasu/k8s-pingpong:3.4-amd && docker push thomastoumasu/k8s-pingpong:3.4-amd
  && cd ../../../
+
+cd the_project/frontend
+IMAGE_TAG="thomastoumasu/k8s-frontend:3.4-amd"
+docker build --platform linux/amd64 --tag $IMAGE_TAG . && docker push $IMAGE_TAG
+docker rmi -f 
