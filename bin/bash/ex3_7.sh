dd0a0a0939351fa60f153579500b375d671501f1
thomastoumasu <thomastoumasu@protonmail.com>
2025-12-17 10:00:43 +0100

    test yaml

--- bin/bash/ex3_7.sh
+++ bin/bash/ex3_7.sh
@@ -1,8 +1,15 @@
 # 3.7 main deployment on namespace project and branch deployment on namespace branch
 
-# # create a static IP
+# # create a static IP - does not work with global gateway
 # gcloud compute addresses create my-ip --region=europe-north1 --network-tier=STANDARD
+# gcloud compute addresses create my-ip --region=europe-north1
 # gcloud compute addresses describe my-ip --region=europe-north1
-# 35.217.26.181
+# # just wait for gateway to allocate an IP and add it to cloudfare DNS record
+# 34.54.85.237
 # use "www.thomastoumasu.dpdns.org" for main aka namespace project
 # use "www.thomastoumasu.xx.kg" for branch aka namespace branch
+
+# debug
+# kubectl describe gateway my-gateway
+# gcloud compute url-maps list
+# kubectl get gateway my-gateway
--- delete_gkecl.sh
+++ delete_gkecl.sh
@@ -1,2 +1,3 @@
 # delete cluster
 gcloud container clusters delete dwk-cluster --zone=europe-north1-b
+gcloud container images delete
